b629e300741afed8cfb964ac89bf1fa1cb35f1d8
Takanori Watanabe <takanori5064@gmail.com>
2018-07-10 18:17:40 +0900

    ラズパイ用にコード書き換え

--- main/speaker.py
+++ main/speaker.py
@@ -8,7 +8,8 @@ VOICE_DIR_PATH = u"./sora_voices/"
 def talk(voice_file_name):
     path = VOICE_DIR_PATH + voice_file_name + '.mp3'
     print('Now playing...')
-    call(['afplay', path]) #This command work on macOS only.
+    #call(['afplay', path]) #This command work on macOS only.
+    call(['aplay', path])
 
 # テストコード
 if __name__ == '__main__':
