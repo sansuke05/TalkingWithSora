536970707e4d900952a6777985681a042b2828fc
Takanori Watanabe <takanori5064@gmail.com>
2018-07-12 02:03:44 +0900

    ラズパイでのmp3再生用コマンドに変更

--- main/speaker.py
+++ main/speaker.py
@@ -9,7 +9,7 @@ def talk(voice_file_name):
     path = VOICE_DIR_PATH + voice_file_name + '.mp3'
     print('Now playing...')
     #call(['afplay', path]) #This command work on macOS only.
-    call(['aplay', path])
+    call(['mpg321', path])
 
 # テストコード
 if __name__ == '__main__':
