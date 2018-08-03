8adf51e858713bae5981b8a75361bebd57da7cf2
Takanori Watanabe <takanori5064@gmail.com>
2018-08-03 18:21:24 +0900

    [add]GPIOの制御処理を追加

--- main/GPIO/gpio_operation.py
+++ main/GPIO/gpio_operation.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import RPi.GPIO as GPIO
 import time
 import threading
 import concurrent.futures
@@ -11,6 +12,10 @@ class GPIOOperation():
         self.stop_event = threading.Event() #停止用のフラグ
         self.thread = None
     
+    def setUp(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(3, GPIO.OUT)
+
     def start_thread(self, mode):
         if mode == 'wait':
             self.thread = threading.Thread(target=self.blink_led)
@@ -20,23 +25,23 @@ class GPIOOperation():
     
     def blink_led(self):
         while not self.stop_event.is_set():
-            print("light on")
+            self.light_on()
             time.sleep(1)
-            print("light off")
+            self.light_off()
             time.sleep(1)
 
     def blink_led_frequently(self):
         while not self.stop_event.is_set():
-            print("light on")
+            self.light_on()
             time.sleep(0.2)
-            print("light off")
+            self.light_off()
             time.sleep(0.2)
     
     def light_on(self):
-        print("light on")
+        GPIO.output(3, True)
     
     def light_off(self):
-        print("light off")
+        GPIO.output(3, False)
     
     def stop(self):
         self.stop_event.set()
