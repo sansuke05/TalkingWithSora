21ceac15d398b0c75c2657f1ac2f971b58533657
Takanori Watanabe <takanori5064@gmail.com>
2018-08-03 16:08:11 +0900

    テストを稼働

--- main/main_operation.py
+++ main/main_operation.py
@@ -5,6 +5,7 @@ import recording_voice
 import transcribe
 import responder
 import speaker
+from gpio.gpio_operation import GPIOOperation
 from system import system_voice
 
 
@@ -27,17 +28,28 @@ def main():
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = ENV_PATH
 
     # LED制御スレッドの生成
+    gpio = GPIOOperation()
+    gpio.start_thread('wait')
 
     while True:
         print('> ', end='')
         command = input()
         # 音声認識開始処理
         if command == 'start':
+            # LEDへの信号を変更
+            gpio.stop()
+            gpio.light_on()
+
             # 音声認識開始音声の再生
-            system_voice.play(command))
+            system_voice.play(command)
 
             # 録音処理
             recording_voice.record()
+
+            # LEDへの信号を変更
+            del gpio
+            gpio = GPIOOperation()
+            gpio.start_thread('recognition')
 
             # サンプリングレートの変換
             command = 'sox recordedVoice/tmp.wav -r 16k recordedVoice/tmp_converted.wav'
@@ -54,12 +66,25 @@ def main():
             response_text = responder.pattern_response(input_text)
             print(response_text)
 
+            # LEDへの信号を変更
+            gpio.stop()
+            gpio.light_on()
+
             # 発話
             speaker.talk(response_text)
+
+            # LEDへの信号を元に戻す
+            del gpio
+            gpio = GPIOOperation()
+            gpio.start_thread('wait')
         
         # 終了条件
         if command == 'exit':
             print('Exiting app...')
+
+            # LEDを消灯
+            gpio.stop()
+            gpio.light_off()
             break
 # [END main]
 
